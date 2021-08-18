b2ac592eb88b0c25661a4b8e29f32673ec60a149
minseongkim <kyrok67@naver.com>
2021-08-19 03:29:11 +0900

    Initial Commit

---
+++ chat-app-firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat-app-firebase
+//
+//  Created by MIN SEONG KIM on 2021/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
