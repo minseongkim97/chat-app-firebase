e6dd4454ac88d93679d66dd71005f661933b2e70
minseongkim <kyrok67@naver.com>
2021-08-19 04:47:24 +0900

    Snapkit 사용 & 로그인 화면 구현.

--- chat-app-firebase/Controllers/ConversationsViewController.swift
+++ chat-app-firebase/Controllers/ConversationsViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 import SnapKit
+import SwiftUI
+
+struct ConversationVCPreView:PreviewProvider {
+    static var previews: some View {
+        ConversationsViewController().toPreview()
+    }
+}
 
 class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false, completion: nil)
+        }
     }
 
 
---
+++ chat-app-firebase/Extension/UIViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewController.swift
+//  chat-app-firebase
+//
+//  Created by MIN SEONG KIM on 2021/08/19.
+//
+
+import UIKit
+import SwiftUI
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+        func toPreview() -> some View {
+            Preview(viewController: self)
+        }
+}
+#endif
+
+
