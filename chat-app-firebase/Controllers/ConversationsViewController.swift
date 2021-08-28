b93e8bc174408ddc2f6665510a55b3e8a794a40a
minseongkim <kyrok67@naver.com>
2021-08-28 21:35:27 +0900

    [ADD] 로그아웃 기능.

--- chat-app-firebase/Controllers/ConversationsViewController.swift
+++ chat-app-firebase/Controllers/ConversationsViewController.swift
@@ -21,7 +21,7 @@ class ConversationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+       
     }
     
     override func viewDidAppear(_ animated: Bool) {
